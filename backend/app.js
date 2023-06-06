5dca1b48b0f6eed1e30ae250f9de506c55436770
An <anaislcno@gmail.com>
2023-06-06 12:46:02 +0200

    set multer

--- backend/app.js
+++ backend/app.js
@@ -27,5 +27,6 @@ app.use(bodyParser.json());
 
 app.use("/api/books", booksRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
